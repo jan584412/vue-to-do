42cb684f5a7373d8ceb653a3e0b99ec0fdb454dc
Chi-Jan <jan584412@gmail.com>
2019-09-17 17:57:03 +0800

    add button

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!',
-    todos: [
-        { text : "The first item", done: true },
-        { text : "The second item", done: false },
-        { text : "The third item" },
-    ]
-  },
-  methods: {
-    toggle: function(todo){
-        todo.done = !todo.done
+    el: '#app',
+    data: {
+        inputValue: null,
+        message: 'Hello Vue!',
+        todos: [
+            { text: "The first item", done: true },
+            { text: "The second item" },
+            { text: "The third item" },
+        ]
+    },
+    methods: {
+        toggle: function (todo) {
+            // console.log("Before", todo.done)
+            todo.done = !todo.done
+            // console.log("After", todo.done)
+        },
+        add: function () {
+            this.todos.push({ text: this.inputValue, done: false });
+            this.inputVaule = "";
+        }
     }
-  }
 })
