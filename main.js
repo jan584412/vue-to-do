8ce46ec69e82f20e9a5f6c1ec5ab5068f4696d1c
Chi-Jan <jan584412@gmail.com>
2019-09-17 17:13:03 +0800

    vue-to-do_ver1.1

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello Vue!',
+    todos: [
+        { text : "The first item", done: true },
+        { text : "The second item", done: false },
+        { text : "The third item" },
+    ]
+  },
+  methods: {
+    toggle: function(todo){
+        todo.done = !todo.done
+    }
   }
 })
